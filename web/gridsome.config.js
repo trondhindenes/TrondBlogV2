63696ebf7271d9e9f74d8d99268ee73d595e8004
Trond Hindenes <trond@hindenes.com>
2020-01-02 14:10:39 +0100

    moar tweak

--- web/gridsome.config.js
+++ web/gridsome.config.js
@@ -13,9 +13,9 @@ const clientConfig = require('./client-config')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
+  siteName: 'Trond\'s Working!',
   siteDescription:
-    'A simple, hackable & minimalistic starter for Gridsome that uses structured content from Sanity.io.',
+    'Trond Hindenes\' blog about computer stuff.',
 
   templates: {
     SanityPost: '/:slug__current'
